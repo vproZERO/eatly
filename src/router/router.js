6e211931e9afda9cb24c7f0ec7858a2b92f3dbd7
abdulahadabduvahobov75@gmail.com <abdulahadabduvahobov75@gmail.com>
2023-10-12 13:34:21 +0500

    aaaa

--- src/router/router.js
+++ src/router/router.js
@@ -1,5 +1,5 @@
 import { createRouter,createWebHashHistory } from "vue-router";
-import Home from '../pages/Home.vue'
+// import Home from '../pages/Home.vue'
 import Dishes from '../pages/dishes.vue'
 import SignIn from '../pages/signin.vue'
 import SignUp from '../pages/signup.vue'
@@ -8,7 +8,7 @@ import Cart from '../pages/cart.vue'
 export  default createRouter({
         history:createWebHashHistory(),
         routes:[
-            {path: '/home',component:Home, alias:'/'},
+            // {path: '/home',component:Home, alias:'/'},
             {path: '/dishes',component:Dishes},
             {path: '/signin', component:SignIn},
             {path: '/signup', component:SignUp},
