b762f8dbb68c28463de53d7e209e2ae9bd7510da
abdulahadabduvahobov75@gmail.com <abdulahadabduvahobov75@gmail.com>
2023-10-22 10:18:10 +0500

    sdsd

--- src/components/get.vue
+++ src/components/get.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="md:py-[50px] md:px-[96px] s:py-[31px] s:px-[25px] mb-[100px]">
-        <div class="get_box lg:w-[1214px] s:mx-auto  bg-purple  relative md:px-[66px] md:pt-[37px] md:pb-[61px] md:rounded-[30px]  sm:w-[503px] s:px-[27px] s:pt-[35px] s:pb-[95px] s:w-[400px] mx-auto    s:rounded-[15px]">
+        <div class="get_box lg:w-[1214px] s:mx-auto  bg-purple  relative md:px-[66px] md:pt-[37px] md:pb-[61px] md:rounded-[30px]  sm:w-[503px] s:px-[27px] s:pt-[35px] s:pb-[95px] s:w-[340px] mx-auto    s:rounded-[15px]">
             <h3 class="uppercase text-white font-extrabold  lg:text-[70px] lg:leading-[105px]  md:text-[49px] md:leading-[74px]  s:text-[31px] s:leading-[62px] s:text-center md:text-start">GET 50%</h3>
             <form class="bg-white py-[7px] pr-[6px] lg:pl-[21px] lg:w-[439px] s:w-[260px] sm:w-[320px]  md:mx-0 s:mx-auto rounded-[14px] flex items-center justify-between">
                 <input class="s:w-[79px] sm:w-[100px] md:w-[217px]" type="email" placeholder=" Enter Your Email Address">
